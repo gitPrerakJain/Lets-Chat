e4838ce58f41efebd16df68feb8e763d0b9fcc68
Prerak Jain <125865568+gitPrerakJain@users.noreply.github.com>
2023-10-01 18:11:40 +0530

    Deployment code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 // const cors = require("cors");
 // const { chats } = require("./data/data");
 const connectDB = require("./config/db");
@@ -19,6 +20,23 @@ app.use(express.json()); // to accept json data
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------Deployment--------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully!");
+  });
+}
+
+// --------Deployment--------
 
 app.use(notFound);
 app.use(errorHandler);
@@ -65,12 +83,12 @@ io.on("connection", (socket) => {
       if (user._id == newMessageReceived.sender._id) return;
 
       socket.in(user._id).emit("message received", newMessageReceived);
-      console.log("inside new mesage");
+      // console.log("inside new mesage handler");
     });
   });
 
   socket.off("setup", () => {
-    console.log("Disconnected user from socket");
+    // console.log("Disconnected user from socket");
     socket.leave(userData._id);
   });
 });
